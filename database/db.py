cecf4c545d2eaf3cf18ee56004f52a99e39dc10f
infoshybyn-dev <info.shybyn@gmail.com>
2026-03-05 09:07:21 +0200

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,1 +1,24 @@
+import aiosqlite
 
+DB = "albion.db"
+
+async def init_db():
+
+    async with aiosqlite.connect(DB) as db:
+
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS prices(
+            item TEXT PRIMARY KEY,
+            price INTEGER
+        )
+        """)
+
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS lfg(
+            message_id INTEGER,
+            role TEXT,
+            user INTEGER
+        )
+        """)
+
+        await db.commit()
