ef10e8fae4cc65a69846f856601a8936506c4060
infoshybyn-dev <info.shybyn@gmail.com>
2026-03-05 09:09:53 +0200

    Update economy.py

--- modules/economy.py
+++ modules/economy.py
@@ -1,1 +1,27 @@
+from config import PREMIUM_TAX, NO_PREMIUM_TAX, SETUP_FEE
 
+
+def craft_profit(
+    item_price,
+    material_cost,
+    station_tax,
+    rra,
+    premium=True
+):
+
+    auction_tax = item_price * (PREMIUM_TAX if premium else NO_PREMIUM_TAX)
+
+    setup_fee = item_price * SETUP_FEE
+
+    return_value = material_cost * rra
+
+    profit = (
+        item_price
+        - auction_tax
+        - setup_fee
+        - material_cost
+        - station_tax
+        + return_value
+    )
+
+    return round(profit)
