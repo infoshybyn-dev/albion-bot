bdb8108cdd6bcac7fdc0f1ae26ad9b39147605c3
infoshybyn-dev <info.shybyn@gmail.com>
2026-03-05 09:58:07 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
-TOKEN = "DISCORD_TOKEN"
+# config.py
 
-PREMIUM_TAX = 0.04
+# Встав свій токен сюди (тільки у тебе, нікому не показуй)
+DISCORD_TOKEN = "MTQ3NzIzODc1MDI2MTE1MzgyMw.GBPOMG.MdFI5GtyoRA21L7_p52jRLHc6BrHi-uhECPKKA"
+
+PREMIUM_TAX = 0.04   # податок з преміумом
 NO_PREMIUM_TAX = 0.08
-SETUP_FEE = 0.025
+SETUP_FEE = 0.025    # комісія на виставку
 
-RRA = {
+RRA = {  # Return Resource Amount залежно від міста
     "Bridgewatch": 0.539,
     "Martlock": 0.367,
     "Lymhurst": 0.367,
