e478607bc97156e45dc726fa111e865f57783d7a
infoshybyn-dev <info.shybyn@gmail.com>
2026-03-05 09:03:57 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,16 @@
-import os
+TOKEN = "DISCORD_TOKEN"
 
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+PREMIUM_TAX = 0.04
+NO_PREMIUM_TAX = 0.08
+SETUP_FEE = 0.025
+
+RRA = {
+    "Bridgewatch": 0.539,
+    "Martlock": 0.367,
+    "Lymhurst": 0.367,
+    "Fort Sterling": 0.367,
+    "Thetford": 0.367,
+    "Caerleon": 0.367
+}
+
+DEFAULT_STATION_TAX = 0.065
