8dc9d36d0ddf6f7622dd5ddf12549e05ce772a4f
infoshybyn-dev <info.shybyn@gmail.com>
2026-03-05 09:10:52 +0200

    Update refine.py

--- modules/refine.py
+++ modules/refine.py
@@ -1,1 +1,14 @@
+def refine_profit(
+    output_price,
+    input_cost,
+    rra,
+    station_tax
+):
 
+    returned = input_cost * rra
+
+    total_cost = input_cost - returned + station_tax
+
+    profit = output_price - total_cost
+
+    return round(profit)
