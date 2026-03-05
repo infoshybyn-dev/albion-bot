bed34f69cc940870b0854cb32e54463988e6cbe9
infoshybyn-dev <info.shybyn@gmail.com>
2026-03-05 10:03:28 +0200

    Update db.py

--- database/db.py
+++ database/db.py
@@ -3,14 +3,11 @@ import aiosqlite
 DB = "albion.db"
 
 async def init_db():
-
     async with aiosqlite.connect(DB) as db:
-
         await db.execute("""
         CREATE TABLE IF NOT EXISTS prices(
             item TEXT PRIMARY KEY,
             price INTEGER
         )
         """)
-
         await db.commit()
