15135244b8257fb805d3d4d601eb3633c6da82e4
infoshybyn-dev <info.shybyn@gmail.com>
2026-03-05 09:11:39 +0200

    Update market.py

--- modules/market.py
+++ modules/market.py
@@ -1,1 +1,18 @@
+import aiohttp
 
+API = "https://west.albion-online-data.com/api/v2/stats/prices/"
+
+async def get_price(item):
+
+    url = API + item
+
+    async with aiohttp.ClientSession() as session:
+
+        async with session.get(url) as r:
+
+            data = await r.json()
+
+            if not data:
+                return None
+
+            return data[0]["sell_price_min"]
